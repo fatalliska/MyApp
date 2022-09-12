ce8171d4fee8e5d4e3df44c9db9bfc4b964d40fa
fatalliska <62687203+fatalliska@users.noreply.github.com>
2022-09-12 14:32:57 +0300

    Create FirstPage.js

---
+++ pages/FirstPage.js
@@ -0,0 +1,71 @@
+import React, {useState} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  Button,
+} from 'react-native';
+
+const FirstPage = ({navigation}) => {
+  const [userName, setUserName] = useState('AboutReact');
+
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <View style={styles.container}>
+        <Text style={styles.textStyle}>
+          Пожалуйста, введите свое имя
+        </Text>
+        {/*Input to get the value from the user*/}
+        <TextInput
+          value={userName}
+          onChangeText={(username) => setUserName(username)}
+          placeholder={'Имя'}
+          style={styles.inputStyle}
+        />
+        {/* On click of the button we will send the data as a Json
+          From here to the Second Screen using navigation */}
+        <Button
+          title="Go Next"
+          //Button Title
+          onPress={() =>
+            navigation.navigate('SecondPage', {
+              paramKey: userName,
+            })
+          }
+        />
+      </View>
+      <Text style={{textAlign: 'center', color: 'grey'}}>
+        www.aboutreact.com
+      </Text>
+    </SafeAreaView>
+  );
+};
+
+export default FirstPage;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    padding: 20,
+  },
+  heading: {
+    fontSize: 25,
+    textAlign: 'center',
+    marginVertical: 10,
+  },
+  textStyle: {
+    textAlign: 'center',
+    fontSize: 16,
+    marginVertical: 10,
+  },
+  inputStyle: {
+    width: '80%',
+    height: 44,
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#DBDBD6',
+  },
+});
